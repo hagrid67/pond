53208f7029b3f68d995b1f885830fa3c7cf4429d
Jeremy Watson <jdhwatson@gmail.com>
2026-07-23 19:27:40 +0100

    Adjust pond rsync deployment behavior

--- scripts/pond-rsync.sh
+++ scripts/pond-rsync.sh
@@ -3,20 +3,23 @@
 # fail fast on error, unset variable, or pipe failure
 set -euo pipefail
 
-echo `date` start rsync 
+echo "$(date) start rsync"
 cd ~/projects/pond
 
-echo rsync from `hostname` to gcweb
+echo "rsync from $(hostname) to gcweb"
 
 cp -f ./output/booking-plot-*.png ./www-root/
 
-echo `date` start rsync gcweb1
+SRC_DIR="./www-root/"
+DEST_DIR="gcweb1:projects/heating/dev/www-root/"
+RSYNC_OPTS=(-av --itemize-changes)
 
-rsync -av ./www-root/index.html gcweb1:projects/heating/dev/www-root
-rsync -av ./www-root/bookings-widget* gcweb1:projects/heating/dev/www-root
-rsync -av ./www-root/bookings.html gcweb1:projects/heating/dev/www-root
-rsync -av ./www-root/booking-plot-*.png gcweb1:projects/heating/dev/www-root
+echo "$(date) preview files to transfer to gcweb1"
+rsync "${RSYNC_OPTS[@]}" --dry-run "$SRC_DIR" "$DEST_DIR"
 
-echo `date` end pond rsync
+echo "$(date) start rsync to gcweb1"
+rsync "${RSYNC_OPTS[@]}" "$SRC_DIR" "$DEST_DIR"
+
+echo "$(date) end pond rsync"
 
 
